6569fcba5d8b79dc51adf51103b71f7ed6bb61e6
Yu <yuthwethwehtet@gmail.com>
2026-09-04 11:12:12 +0800

    Interactive profile creator

---
+++ interactiveprofile.py
@@ -0,0 +1,9 @@
+username = input("Enter your username: ")
+age = int(input("Enter your age: "))
+category = input("Enter your category: ")
+
+print("\nInstagram Profile")
+print("====================================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
