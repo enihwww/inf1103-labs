198defa0d8f415fc8dd4dca36288deb8c9b9ff37
Yu <yuthwethwehtet@gmail.com>
2026-09-04 11:00:43 +0800

    Track the growth follower

---
+++ followertracker.py
@@ -0,0 +1,11 @@
+followers = 100
+
+followers += 50  # Increase followers by 50
+print("Day 1:", followers)
+
+followers += 20  # Increase followers by 20
+print("Day 2:", followers)
+
+followers -= 10  # Decrease followers by 10
+print("Day 3:", followers)
+
